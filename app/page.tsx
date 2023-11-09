383bee093c4225032d932be0243ee46eb7b0263e
vini <velkhaliliborsato@gmail.com>
2023-11-08 22:08:50 -0300

    :3

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>CiroLiro!</h1>
+      <h2>:3 :3 :3</h2>
+      <Image src={"/ciroliro.jpg"} width={500} height={500} alt={""} style={{objectFit:"contain"}}/>
     </main>
   )
 }
